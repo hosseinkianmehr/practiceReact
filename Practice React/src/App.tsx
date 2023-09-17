d9af0198fba940b5dca6fec162e7024672fcac16
Hossein <hosseinkianmehr@yahoo.com>
2023-09-17 19:28:36 +0330

    fix login token and locol storage

--- Practice React/src/App.tsx
+++ Practice React/src/App.tsx
@@ -2,14 +2,31 @@
 import { ThemeProvider } from '@emotion/react'
 import './App.css'
 import { Page } from './page'
-import { useAppSelector } from './store/hook'
+import { useAppDispatch, useAppSelector } from './store/hook'
 import { getTheme } from './theme'
 import {
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
+import { authActions } from './store/auth'
+import { useJwt } from 'react-jwt';
 
 function App() {
+  const token = localStorage.getItem("token");
+  console.log(token,'tppppp')
+  const dispatch = useAppDispatch()
+ console.log(token !== null ,'token !== null')
+  if (token !== null) {
+    const { isExpired , decodedToken } = useJwt(token)
+    console.log(isExpired , decodedToken,'isExpired , decodedToken')
+    console.log("0")
+    if (isExpired) {
+      dispatch(authActions.login(decodedToken))
+      console.log("1")
+    }
+  }
+  
+
   const select = useAppSelector(state => state.theme.dark)
   const theme = getTheme(select)
   const queryClient = new QueryClient()
--- Practice React/src/store/auth.ts
+++ Practice React/src/store/auth.ts
@@ -2,42 +2,55 @@ import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import Login from "../page/authentication/login";
 import axios from "axios";
 interface ServerResponse<T> {
-    accessToken: string,
-    user: object
-    status: string
-    message: string
-    data: T
- }
- 
- interface loginSuccessData {
-    accessToken: string,
-    user: object
- }
- 
- type loginSuccessResponse = ServerResponse<loginSuccessData>
-/////////////////////
-export const login = createAsyncThunk("auth/login", async (data:object) => {
-  console.log(data,'asfdsad')
-    const response = await axios.post('https://fakestoreapi.com/auth/login', data)
-    console.log(response.data, 'actionssss')
-    return response.data
- })
+  accessToken: string,
+  user: object
+  status: string
+  message: string
+  data: T
+}
 
-const initialState ={
-    token : '',
-    istoken : false
-} 
+interface loginSuccessData {
+  Token: string,
+  user?: object
+}
+
+type loginSuccessResponse = ServerResponse<loginSuccessData>
+/////////////////////
+export const login = createAsyncThunk("auth/login", async (data: object) => {
+  console.log(data, 'asfdsad')
+  const response = await axios.post('https://fakestoreapi.com/auth/login', data)
+  console.log(response.data, 'actionssss')
+  return response.data
+})
+
+const initialState = {
+  token: '',
+  istoken: false,
+  user : {}
+}
 export const authslice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-      builder
-        .addCase( login, (state, action:PayloadAction<loginSuccessResponse>) => {
-          state.token = action,
-          state.istoken = true
-        })
-       
+  name: 'auth',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, action: PayloadAction<loginSuccessResponse>) => {
+        state.token = action.payload.token,
+        state.istoken = true;
+        localStorage.setItem("token", action.payload.token)
+      })
+
+  }
+  ,
+  reducers: {
+    logout: () => {
+      localStorage.removeItem("token");
     },
-  })
-  export const authActions = authslice.actions
+    login: (state, action) => {
+      state.istoken = true;
+      state.user = action.payload
+      console.log(action.payload,'action.payload.decodedToken')
+    }
+  }
+
+})
+export const authActions = authslice.actions
