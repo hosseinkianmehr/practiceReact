feb6642ce038d9546ea66522f50bde9e478a8cc6
Hossein <hosseinkianmehr@yahoo.com>
2023-09-17 20:00:45 +0330

    fix header =>login and logout

--- Practice React/src/component/header/header.tsx
+++ Practice React/src/component/header/header.tsx
@@ -8,13 +8,23 @@ import Switch from '@mui/material/Switch';
 import { FormControlLabel } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../store/hook';
 import { themeStore } from '../../store/theme';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { authActions } from '../../store/auth';
 
 export const Header = () => {
+  const userName = useAppSelector((state) => state.auth.user.user)
+  const islogin = useAppSelector((state) => state.auth.islogin)
   const dispatch = useAppDispatch()
   const select = useAppSelector(state => state.theme.dark)
 const handelDark =()=>{
   dispatch(themeStore.dark({dark:select?false:true}));}
+  const handlelogout =()=>{
+    dispatch(authActions.logout())
+  }
+  const navigate = useNavigate()
+  const handlelogin =()=>{
+    navigate('/login')
+  }
   return (
     <AppBar position="fixed" color="primary">
       <Toolbar>
@@ -29,8 +39,18 @@ const handelDark =()=>{
             <MenuIcon />
           </IconButton>
           <FormControlLabel onChange={handelDark} control={<Switch defaultChecked />} label="Label" />
+          {islogin?<>
+          <Typography>{userName}</Typography>
+          <Button onClick={handlelogout} color="inherit">logout</Button>
+          </>
+          :
+          <Button onClick={handlelogin} color="inherit">login</Button>}
+          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          
+          </Typography>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            Shop
           </Typography>
           <Link to={''}>
           <Button color="inherit">best shop</Button>
